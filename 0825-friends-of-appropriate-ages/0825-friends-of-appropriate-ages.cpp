9b24fdbeccdf4dfb20ba79be43dad55d39394f9a
Astitva Maurya <144588102+Astitva0809@users.noreply.github.com>
2024-10-02 16:49:25 +0530

    Time: 42 ms (46.28%), Space: 41.2 MB (43.55%) - LeetHub

---
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        sort(ages.begin(),ages.end());
+        int r = ages.size()-1;
+        int l = ages.size()-2;
+        int tot = 0;
+        int streak = 1;
+        while(r > 0){
+            while((l>=r) || l >= 0 && ages[l] > 0.5 * ages[r]+7){
+                if(ages[l] == ages[l+1] && ages[l] > 14){
+                    streak++;
+                }
+                else{
+                    tot+= streak*(streak-1)/2;
+                    streak = 1;
+                }
+                l--;
+            }
+            l++;
+            tot+= r-l;
+            r--;
+            l--;
+        }
+        tot+= streak*(streak-1)/2;
+        return tot;
+    }
+};
