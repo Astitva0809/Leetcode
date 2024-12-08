5711fd35ca3ffc677e29fd255170fabe08774d5d
Astitva Maurya <144588102+Astitva0809@users.noreply.github.com>
2024-12-08 16:16:24 +0530

    Time: 3 ms (28.62%), Space: 17.1 MB (49.42%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -22,7 +22,7 @@ public:
            temp.pop_back();
            return;
         }
-        // cout << root -> val << endl;
+      
         temp.push_back(root->val);
         helper(root->left, temp,ans);
         helper(root->right, temp,ans);
