5648a190dd44bf0de8946dd272eea0543eed41f1
Astitva Maurya <144588102+Astitva0809@users.noreply.github.com>
2025-06-12 11:38:17 +0530

    Time: 0 ms (100%), Space: 30.5 MB (23.04%) - LeetHub

--- 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/3747-maximum-difference-between-adjacent-elements-in-a-circular-array.cpp
+++ 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/3747-maximum-difference-between-adjacent-elements-in-a-circular-array.cpp
@@ -9,7 +9,9 @@ public:
         }
             for(int i=1;i<n;i++){
             int maxs=nums[i-1]-nums[i];
-            if(maxi<maxs)maxi=maxs;
+        if(maxi<maxs)maxi=maxs;
+        
+        
         }
 
         int z=max(nums[n-1]-nums[0],nums[0]-nums[n-1]);
